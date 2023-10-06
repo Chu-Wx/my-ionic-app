1267a94c8e3679d601c31dbfdbff227302281abd
Chuwei Xu <chuweismail@gmail.com>
2023-10-05 23:35:30 -0400

    base proj + input + showing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PopoverComponent } from './popover/popover.component';
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, PopoverComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { IonInput, PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  selectedSeg: string = 'Grocery';
+  Category :Array<string> = [];
+  metadata: Array<Array<string | number>> = [];
+  @ViewChild('categoryInput')
+  categoryInput!: IonInput;
+  @ViewChild('titleInput') titleInput!: IonInput;
+  @ViewChild('amountInput') amountInput!: IonInput;
+  constructor(private popover: PopoverController) {}
 
-  constructor() {}
+  addData() {
+    // Get values from Ion inputs
+    const category = this.categoryInput.value;
+    const title = this.titleInput.value;
+    const amount = this.amountInput.value;
 
+    console.log('Category:', category);
+    console.log('Title:', title);
+    console.log('Amount:', amount);
+
+    // Check if any of the inputs are undefined or null
+    if (category === null || category === undefined) {
+      // Handle the case where category is missing
+      console.log('Category is missing.');
+      return;
+    }
+
+    // Check if title and amount are of type string
+    if (typeof title !== 'string' || typeof amount !== 'string' || typeof category !== 'string') {
+      // Handle the case where title or amount is not a string
+      console.log('Title or amount is not a string.');
+      return;
+    }
+
+    // Check if the category already exists in the category array
+  if (!this.Category.includes(category)) {
+    // If it doesn't exist, add it to the category array
+    this.Category.push(category);
+  }
+
+    this.metadata.push([category, title, amount]);
+
+    console.log('Updated Metadata:', this.metadata);
+    this.popover.dismiss();
+  }
 }
